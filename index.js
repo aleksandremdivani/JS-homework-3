8caea575ed2fd7ff11011c6a325cca7b9e3f706c
aleksandremdivani <aleksandre.mdivani@geolab.edu.ge>
2025-11-29 15:07:59 +0400

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,24 @@ const guests = ["Anna", "", "George", "", "Saba", "Nick", ""];
 const cleanList = () => {
   let cleaned = [];
   for (let i = 0; i < guests.length; i++) {
-    if (guests[i] !== ""){
-        cleaned.splice(guests.indexOf(guests[i]), 0, guests[i]);
+    if (guests[i] !== "") {
+      cleaned.splice(guests.indexOf(guests[i]), 0, guests[i]);
     }
   }
   return cleaned;
 };
 const filteredGuests = cleanList();
 console.log(filteredGuests);
+
+const names = ["Mary", "Luka", "Levan", "Tako", "Giorgi", "Ana", "Salome"];
+const removeNamesWithLetter = (arr, letter) => {
+  let filteredNames = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if ((!arr[i].toLowerCase().includes(letter.toLowerCase()))) {
+      filteredNames.push(arr[i]);
+    }
+  }
+  return filteredNames;
+};
+const resultNames = removeNamesWithLetter(names, "m");
+console.log(resultNames);
